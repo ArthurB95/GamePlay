b34e2a2498d670699edf46454c98b8dc62dfbeaa
Arthur Bicalho <arthurbic95@gmail.com>
2021-07-03 12:39:00 -0300

    GamePlay

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import DiscordImg from '../../assets/discord.png'
+
+import {RectButton, RectButtonProps} from 'react-native-gesture-handler'
+
+import { Text } from 'react-native';
+
+import { styles } from './styles';
+
+type Props = RectButtonProps & {
+  title?: string
+}
+
+export function Button({title, ...rest} : Props) {
+
+  return (
+
+    <RectButton style={styles.container} {...rest}>
+
+      <Text style={styles.title}>
+
+        {title}
+
+      </Text>
+
+    </RectButton>
+
+  )
+
+}
